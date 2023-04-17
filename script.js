b2dfdb625fa9fa7712c2782a1d185015b47e56f6
sulin1234 <130384660+sulin1234@users.noreply.github.com>
2023-04-18 01:20:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,118 @@
-var num1 = '';
-var num2 = '';
-var operator = '';
-var result = '';
+let output = document.getElementById("output");
+let clear = document.getElementById("clear");
+let equals = document.getElementById("equals");
 
-function display(num) {
-  if (result != '') {
-    clearResult();
+let operator = null;
+let operand1 = null;
+let operand2 = null;
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(operator, operand1, operand2) {
+  let result = null;
+
+  switch (operator) {
+    case "+":
+      result = add(operand1, operand2);
+      break;
+    case "-":
+      result = subtract(operand1, operand2);
+      break;
+    case "*":
+      result = multiply(operand1, operand2);
+      break;
+    case "/":
+      result = divide(operand1, operand2);
+      break;
   }
-  if (operator == '') {
-    num1 += num;
-    document.getElementsByName('result')[0].value = num1;
+
+  return result;
+}
+
+function updateOutput() {
+  if (operand2 === null) {
+    output.textContent = operand1;
   } else {
-    num2 += num;
-    document.getElementsByName('result')[0].value = num2;
+    output.textContent = operand2;
   }
 }
 
-function calculate(op) {
-  if (op == '=') {
-    if (num2 == '') {
-      result = num1;
+function handleClear() {
+  operator = null;
+  operand1 = null;
+  operand2 = null;
+
+  updateOutput();
+}
+
+function handleNumberClick(event) {
+  let number = event.target.textContent;
+
+  if (operator === null) {
+    if (operand1 === null) {
+      operand1 = number;
     } else {
-      result = eval(num1 + operator + num2);
+      operand1 += number;
     }
+
+    output.textContent = operand1;
+  } else {
+    if (operand2 === null) {
+      operand2 = number;
+    } else {
+      operand2 += number;
+    }
+
+    output.textContent = operand2;
+  }
+}
+
+function handleOperatorClick(event) {
+  let newOperator = event.target.textContent;
+
+  if (operator === null) {
+    operator = newOperator;
+  } else {
+    operand1 = operate(operator, parseFloat(operand1), parseFloat(operand2));
+    operand2 = null;
+    operator = newOperator;
+    output.textContent = operand1;
+  }
+}
+
+function handleEqualsClick() {
+  if (operand1 !== null && operand2 !== null && operator !== null) {
+    operand1 = operate(operator, parseFloat(operand1), parseFloat(operand2));
+    operand2 = null;
+    operator = null;
+    updateOutput();
+  }
+}
+
+clear.addEventListener("click", handleClear);
+
+let numbers = document.querySelectorAll(".number");
+numbers.forEach(function(number) {
+  number.addEventListener("click", handleNumberClick);
+});
+
+let operators = document.querySelectorAll(".operator");
+operators.forEach(function(operator) {
+  operator.addEventListener("click", handleOperatorClick);
+});
+
+equals.addEventListener("click", handleEqualsClick);
